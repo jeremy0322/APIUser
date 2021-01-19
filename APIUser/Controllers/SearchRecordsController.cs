28a96f9719cb5fc7a24c8114bcd9f906c6423d4d
Kok Bin <kokbin2335@hotmail.com>
2021-01-19 14:42:10 +0800

    Add project files.

---
+++ APIUser/Controllers/SearchRecordsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using APIUser.Models;
+
+namespace APIUser.Controllers
+{
+    public class SearchRecordsController : Controller
+    {
+        // GET: SearchRecords
+        public ActionResult Index(string search)
+        {
+            IEnumerable<user> obj = null;
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://localhost:44325/api/");
+
+            var consumeapi = hc.GetAsync("User?search=" + search);
+            consumeapi.Wait();
+
+            var readdata = consumeapi.Result;
+            if (readdata.IsSuccessStatusCode)
+            {
+                var displaydata = readdata.Content.ReadAsAsync<IList<user>>();
+                displaydata.Wait();
+                obj = displaydata.Result;
+            }
+            return View(obj);
+        }
+    }
+}
---
+++ APIUser/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using APIUser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIUser.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IHttpActionResult getUserEmail(string search)
+        {
+            DBModel sd = new DBModel();
+            var result = sd.users.Where(x => x.email.Contains(search) || search == null).ToList();
+            return Ok(result);
+        }
+    }
+}
